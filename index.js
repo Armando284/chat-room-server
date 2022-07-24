9479ad66791fc5aefb2a874012df8820e613d709
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-07-24 18:35:38 -0400

    Adding new origin to cors

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const io = require('socket.io')(PORT, {
         origin: [
             "http://127.0.0.1:5500",
             "http://localhost:8080",
-            "https://admin.socket.io"
+            "https://admin.socket.io",
+            "https://chat-room-web-application.vercel.app"
         ]
     }
 })
